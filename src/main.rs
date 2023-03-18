bff1bfd80607d9fd6d4c192b0155527de4d5ae36
ekrasmussen <emil@erasmussen.dk>
2023-03-18 16:31:08 +0100

    Added conversion to bytes

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,30 @@ const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
 const BLOCK_SIZE: u32 = 12;
 
-const GRIDX: u32 = WIDTH / SIZE;
-const GRIDY: u32 = HEIGHT / SIZE;
+const GRIDX: u32 = WIDTH / BLOCK_SIZE;
+const GRIDY: u32 = HEIGHT / BLOCK_SIZE;
+
+
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    for i in 1..args.len() {
+        println!("File: {}", args[i]);
+
+        let binary_data = match convert_to_binary(&args[i]) {
+            Ok(data) => data,
+            Err(e) => {
+                println!("Error loading file, check file path again");
+                return;
+            }
+        };
+    }
+}
+
+fn convert_to_binary(path: &str) -> Result<Vec<u8>, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
 }
