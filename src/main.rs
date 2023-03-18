3f09247946dec71d1a69b5484363fa739677ca74
ekrasmussen <emil@erasmussen.dk>
2023-03-18 16:24:03 +0100

    Added constants for image generation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
+const WIDTH: u32 = 1920;
+const HEIGHT: u32 = 1080;
+const BLOCK_SIZE: u32 = 12;
 
+const GRIDX: u32 = WIDTH / SIZE;
+const GRIDY: u32 = HEIGHT / SIZE;
 
 fn main() {
     println!("Hello, world!");
