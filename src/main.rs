8a36723b351409a10fb0ba741876840996eb21d2
ekrasmussen <emil@erasmussen.dk>
2023-03-21 15:15:40 +0100

    Added testing mode with log function

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use std::fs;
 use std::path::Path;
 use std::thread;
 use std::time::Instant;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 
 const WIDTH: usize = 1920;
 const HEIGHT: usize = 1080;
@@ -24,62 +27,65 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args[1] == "test" {
-        let test_files = vec!["50KB.bin", "100KB.bin", "250KB.bin", "500KB.bin", "750KB.bin", "1MB.bin", "2MB.bin", "4MB.bin", "5MB.bin", "10MB.bin", "20MB.bin", "40MB.bin", "50MB.bin", "100MB.bin", "250MB.bin", "500MB.bin" "1GB.bin"];
+        let test_files = vec!["50KB.bin", "100KB.bin", "250KB.bin", "500KB.bin", "750KB.bin", "1MB.bin", "2MB.bin", "4MB.bin", "5MB.bin", "10MB.bin", "20MB.bin", "40MB.bin", "50MB.bin", "100MB.bin", "250MB.bin", "500MB.bin", "1GB.bin"];
 
+        let test_string = "test \n test \n test";
 
+        create_log_file(test_string);
     }
-
-    for i in 1..args.len() {
-        println!("File: {}", args[i]);
-
-        //Convert to binary
-        let binary_data = match convert_to_binary(&args[i]) {
-            Ok(data) => data,
-            Err(e) => {
-                println!("Error loading file, check file path again");
-                return;
-            }
-        };
-
-        let mut image_values = imgedit::generate_image_filestream_colored(binary_data);
-
-        let chunk_size = image_values.len() / THREADS;
-        let remainder = image_values.len() % THREADS;
-
-        let mut threads = vec![];
-
-        let mut start = 0;
-        for i in 0..THREADS {
-            let mut end = start + chunk_size;
-            if i == THREADS - 1 {
-                end += remainder;
-            }
-
-            let values_chunk = image_values[start..end].to_vec();
-            let thread_handle = thread::spawn(move || {
-                for (idx, value) in values_chunk.iter().enumerate() {
-                    let image_index = start + idx;
-                    imgedit::create_image_colored(*value, image_index);
+    else {
+        for i in 1..args.len() {
+            println!("File: {}", args[i]);
+    
+            //Convert to binary
+            let binary_data = match convert_to_binary(&args[i]) {
+                Ok(data) => data,
+                Err(e) => {
+                    println!("Error loading file, check file path again");
+                    return;
                 }
-            });
-
-            threads.push(thread_handle);
-
-            start = end;
+            };
+    
+            let mut image_values = imgedit::generate_image_filestream_colored(binary_data);
+    
+            let chunk_size = image_values.len() / THREADS;
+            let remainder = image_values.len() % THREADS;
+    
+            let mut threads = vec![];
+    
+            let mut start = 0;
+            for i in 0..THREADS {
+                let mut end = start + chunk_size;
+                if i == THREADS - 1 {
+                    end += remainder;
+                }
+    
+                let values_chunk = image_values[start..end].to_vec();
+                let thread_handle = thread::spawn(move || {
+                    for (idx, value) in values_chunk.iter().enumerate() {
+                        let image_index = start + idx;
+                        imgedit::create_image_colored(*value, image_index);
+                    }
+                });
+    
+                threads.push(thread_handle);
+    
+                start = end;
+            }
+    
+    
+            //waiting for threads to finish
+            for thread_handle in threads {
+                thread_handle.join().unwrap();
+            }
+            // for i in 0..image_values.len() {
+            //     imgedit::create_image_colored(image_values[i], i);
+            // }
+            println!("Total amount of images: {}", image_values.len());
         }
-
-
-        //waiting for threads to finish
-        for thread_handle in threads {
-            thread_handle.join().unwrap();
-        }
-        // for i in 0..image_values.len() {
-        //     imgedit::create_image_colored(image_values[i], i);
-        // }
-        println!("Total amount of images: {}", image_values.len());
+        let elapsed = now.elapsed();
+        println!("Elapsed: {:.2?}", elapsed);
     }
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
 }
 
 fn create_directory(path: &str) -> Result<(), std::io::Error> {
@@ -93,9 +99,13 @@ fn convert_to_binary(path: &str) -> Result<Vec<u8>, std::io::Error> {
     Ok(buffer)
 }
 
-fn convert_to_text(path: &str, data: &[u8]) -> Result<(), std::io::Error> {
-    let mut file = File::create(path)?;
-    file.write_all(data)?;
+fn create_log_file(content: &str) -> Result<(), std::io::Error> {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("log.txt")?;
+
+    file.write_all(content.as_bytes())?;
     Ok(())
 }
 
