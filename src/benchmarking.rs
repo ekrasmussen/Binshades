c13335905db30eef3eb7a000368e355392265a29
ekrasmussen <emil@erasmussen.dk>
2023-03-21 16:51:06 +0100

    Finalized benchmarking options

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -5,9 +5,21 @@ use std::fs::OpenOptions;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+use std::time::Duration;
 
 pub fn create_log_entry() {
+    let text = format!("####################\nEntry: {}\n####################\n", get_current_time());
+    write_log(&text);
+}
 
+pub fn add_benchmark(filename: &str, filestream: Duration, image: Duration, total: Duration) {
+    let text = format!("Data for file: {}\n    Filestream time: {:.2}\n    Image Write Time: {:.2}\n    Total Time: {:.2}\n\n", filename, filestream.as_secs_f64(), image.as_secs_f64(), total.as_secs_f64());
+    write_log(&text);
+}
+
+pub fn finalize_log(total_time: Duration) {
+    let text = format!("Benchmark over, total time was {:.2}\n\n", total_time.as_secs_f64());
+    write_log(&text);
 }
 
 pub fn write_log(content: &str) -> Result<(), std::io::Error> {
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod benchmarking;
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::io;
 use std::fs;
 use std::thread;
 use std::time::Instant;
@@ -14,7 +15,7 @@ const WIDTH: usize = 1920;
 const HEIGHT: usize = 1080;
 const BLOCK_SIZE: usize = 12;
 const THREADS: usize = 4;
-
+const EPOCHS: usize = 3;
 const GRIDX: usize = WIDTH / BLOCK_SIZE;
 const GRIDY: usize = HEIGHT / BLOCK_SIZE;
 
@@ -22,27 +23,51 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args[1] == "test" {
-        let test_files = vec!["50KB.bin", "100KB.bin", "250KB.bin", "500KB.bin", "750KB.bin", "1MB.bin", "2MB.bin", "4MB.bin", "5MB.bin", "10MB.bin", "20MB.bin", "40MB.bin", "50MB.bin", "100MB.bin", "250MB.bin", "500MB.bin", "1GB.bin"];
+        for i in 0..EPOCHS {
+            let test_files = vec!["50KB.bin", "100KB.bin", "250KB.bin", "500KB.bin", "750KB.bin", "1MB.bin", "2MB.bin", "4MB.bin", "5MB.bin", "10MB.bin", "20MB.bin", "40MB.bin", "50MB.bin", "100MB.bin", "250MB.bin", "500MB.bin", "1GB.bin"];
 
-        benchmarking::create_log_entry();
-
-        for i in 0..test_files.len() {
-            let now = Instant::now();
-
-            let elapsed = now.elapsed();
-            println!("Elapsed: {:.2?}", elapsed);
+            benchmarking::create_log_entry();
+            let time_now = Instant::now();
+            for i in 0..test_files.len() {
+                let now = Instant::now();
+                
+                println!("Converting file to vector filestream...");
+                
+                let color_values = file_to_image(test_files[i]);
+                let after_filestream = now.elapsed();
+                
+                println!("Writing Images...");
+                create_images(color_values);
+                let total = now.elapsed();
+    
+                let after_images = total - after_filestream;
+    
+                println!("Filestream time: {:.2?}", after_filestream);
+                println!("Image generation time: {:.2?}", after_images);
+                println!("Total: {:.2?}", total);
+                benchmarking::add_benchmark(test_files[i], after_filestream, after_images, total);
+                delete_directory("output");
+            }
+    
+            benchmarking::finalize_log(time_now.elapsed());
         }
     }
     else {
         for i in 1..args.len() {
             println!("File: {}", args[i]);
-
+            println!("Converting file to vector filestream...");
+            
+            let color_values = file_to_image(&args[i]);
+            
+            println!("Writing Images...");
+            create_images(color_values);
+            println!("Done");
         }
     }
 }
 
-fn create_directory(path: &str) -> Result<(), std::io::Error> {
-    fs::create_dir_all(path)
+fn delete_directory(path: &str) -> io::Result<()> {
+    fs::remove_dir_all(path)
 }
 
 fn convert_to_binary(path: &str) -> Result<Vec<u8>, std::io::Error> {
@@ -58,20 +83,26 @@ fn print_image_as_text(array: &[[u8; GRIDY]; GRIDX]) {
         print!("{:3} ", array[j][i]);
     }
     println!();
+    }
 }
 
-fn file_to_image(filepath: &str) {
+
+fn file_to_image(filepath: &str) -> Vec<[[[u8; 3]; GRIDY]; GRIDX]> {
     //Convert to binary
     let binary_data = match convert_to_binary(&filepath) {
         Ok(data) => data,
         Err(e) => {
             println!("Error loading file, check file path again");
-            return;
+            return Vec::new();
         }
     };
 
     let mut image_values = imgedit::generate_image_filestream_colored(binary_data);
+    // create_images(image_values);
+    image_values
+}
 
+fn create_images(image_values: Vec<[[[u8; 3]; GRIDY]; GRIDX]>) {
     let chunk_size = image_values.len() / THREADS;
     let remainder = image_values.len() % THREADS;
 
@@ -106,5 +137,4 @@ fn file_to_image(filepath: &str) {
     //     imgedit::create_image_colored(image_values[i], i);
     // }
     println!("Total amount of images: {}", image_values.len());
-    }
 }
